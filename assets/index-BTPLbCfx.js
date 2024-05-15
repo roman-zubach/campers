d28eca73925ec9cfc8c8bbdcb22b2a29147b8854
djukipuki <Angel309933gmail.com>
2024-05-16 00:55:34 +0300

    Updates

---
+++ assets/index-BTPLbCfx.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-ZQSIfa_i.js";const e=()=>t.jsx("div",{children:"Index"});export{e as default};
---
+++ assets/index-DazWCd56.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-ZQSIfa_i.js";const s=()=>r.jsx("div",{children:"Favorites"});export{s as default};
