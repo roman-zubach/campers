93a168f4d2e2277c57592df3f237562f2002d4a4
djukipuki <Angel309933gmail.com>
2024-05-17 21:44:00 +0300

    Updates

--- assets/index-Bw-cssMR.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-DLF4_IL7.js";const s=()=>r.jsx("div",{children:"Favorites"});export{s as default};
---
+++ assets/index-y1zR8Ypz.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-BzAeYMIx.js";const s=()=>r.jsx("div",{children:"Favorites"});export{s as default};
