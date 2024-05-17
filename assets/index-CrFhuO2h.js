52e117ef9b8a4cc43d9228715ac601e2a99b08f5
djukipuki <Angel309933gmail.com>
2024-05-17 19:58:56 +0300

    Updates

---
+++ assets/index-CrFhuO2h.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-H41T4ees.js";const s=()=>r.jsx("div",{children:"Favorites"});export{s as default};
--- assets/index-DIMNma5A.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-i5twSqbF.js";const s=()=>r.jsx("div",{children:"Favorites"});export{s as default};
