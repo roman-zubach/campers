aaf579ffa01ea8ff679da5e8baca887caacd97ea
djukipuki <Angel309933gmail.com>
2024-05-17 21:01:57 +0300

    Updates

---
+++ assets/index-Bw-cssMR.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-DLF4_IL7.js";const s=()=>r.jsx("div",{children:"Favorites"});export{s as default};
--- assets/index-CrFhuO2h.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-H41T4ees.js";const s=()=>r.jsx("div",{children:"Favorites"});export{s as default};
