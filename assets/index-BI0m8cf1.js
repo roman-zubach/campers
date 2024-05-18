994550bea0367bd028ea0beb58b90b108426bdae
rzubach <Angel309933gmail.com>
2024-05-18 17:17:11 +0300

    Updates

---
+++ assets/index-BI0m8cf1.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-BS_VaL8k.js";const s=()=>r.jsx("div",{children:"Favorites"});export{s as default};
--- assets/index-Gv6Zlgj6.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-zbo8PIe9.js";const s=()=>r.jsx("div",{children:"Favorites"});export{s as default};
