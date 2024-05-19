31abc7188ae4fb100b07482f88cefd02042d0304
roman-zubach <Angel309933gmail.com>
2024-05-19 13:16:39 +0300

    Updates

--- assets/index-BI0m8cf1.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-BS_VaL8k.js";const s=()=>r.jsx("div",{children:"Favorites"});export{s as default};
---
+++ assets/index-BdRdaKAM.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-A6-ow6Ss.js";const s=()=>r.jsx("div",{children:"Favorites"});export{s as default};
